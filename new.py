6f9fbda7e707266c38a0bddf64059355c0d70b3a
grvkmr07 <grvkmr07121998@gmail.com>
2024-04-06 21:36:18 +0530

    something

--- new.py
+++ new.py
@@ -7,6 +7,6 @@ from pyspark import SparkContext, SparkConf
 path = r'/Users/grvkmr07/Documents'
 spark = SparkSession.builder.appName("ABC").getOrCreate()
 for file in os.listdir(path):
-    spk =spark.read.text(os.path.join(path, file))
+    spk = spark.read.text(os.path.join(path, file))
     print(f"{file}: Total Count: {spk.count()}")
 
